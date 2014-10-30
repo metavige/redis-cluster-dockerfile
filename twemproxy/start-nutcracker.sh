3ef292221c47270ba81c29ddc38e3ac1bddc2295
metavige <metavige@gmail.com>
2014-10-30 10:53:19 +0800

    move nutcracker.conf to nutcracker.yml

--- twemproxy/start-nutcracker.sh
+++ twemproxy/start-nutcracker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sed -i "s/{REDIS_HOST}/$REDIS_PORT_6379_TCP_ADDR/g" /etc/nutcracker/nutcracker.yml
-sed -i "s/{REDIS_POST}/$REDIS_PORT_6379_TCP_PORT/g" /etc/nutcracker/nutcracker.yml
+sed -i "s/{REDIS_PORT}/$REDIS_PORT_6379_TCP_PORT/g" /etc/nutcracker/nutcracker.yml
 
 nutcracker -d -c /etc/nutcracker/nutcracker.yml -o /var/log/nutcracker.log
