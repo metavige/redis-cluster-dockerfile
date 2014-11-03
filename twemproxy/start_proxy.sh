851e5d11ec2f5b9f8f412e6dd9b5a4f4e9c50aaf
metavige <metavige@gmail.com>
2014-11-03 11:11:44 +0800

    move run twemproxy shell to start_proxy.sh

--- twemproxy/start_proxy.sh
+++ twemproxy/start_proxy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-TWEMPROXY_PORT=6100
-TWEMPROXY_STATS_PORT=6100
-ETCD_HOST=127.0.0.1:4001
+TWEMPROXY_PORT=6000
+TWEMPROXY_STATS_PORT=6001
+# ETCD_HOST=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'):4001
+ETCD_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' etcd):4001
 
 etcdctl set /services/twemproxy/port $TWEMPROXY_PORT
-docker run --name=redis-twemproxy --rm -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6001 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
+docker rm -f redis-twemproxy
+
+docker run -d --name=redis-twemproxy -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
 etcdctl set /services/twemproxy/host 127.0.0.1
