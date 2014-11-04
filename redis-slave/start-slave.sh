a575158910708c38e52dea936b3f5d6482c144c8
metavige <metavige@gmail.com>
2014-11-04 11:32:39 +0800

    return use redis-server self, not supervisord. cause supervisord+redis cannot connect to 6379

--- redis-slave/start-slave.sh
+++ redis-slave/start-slave.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# redis-server --slaveof $REDIS_PORT_6379_TCP_ADDR $REDIS_PORT_6379_TCP_PORT
+redis-server --slaveof $REDIS_PORT_6379_TCP_ADDR $REDIS_PORT_6379_TCP_PORT
 
 # Run docker run with --link redis-master:redis
-if [ -n "${REDIS_PORT_6379_TCP_ADDR:+x}" ]; then
-  mv /etc/supervisor/supervisord.conf /tmp/supervisord.conf
-  sed -e "/confd -node/s/127.0.0.1/${REDIS_PORT_6379_TCP_ADDR}/" /tmp/supervisord.conf > /etc/supervisor/supervisord.conf
-fi
+#if [ -n "${REDIS_PORT_6379_TCP_ADDR:+x}" ]; then
+#  mv /etc/supervisor/supervisord.conf /tmp/supervisord.conf
+#  sed -e "/confd -node/s/127.0.0.1/${REDIS_PORT_6379_TCP_ADDR}/" /tmp/supervisord.conf > /etc/supervisor/supervisord.conf
 
-/root/bin/run.sh 
+#fi
+
+#/root/bin/run.sh 
