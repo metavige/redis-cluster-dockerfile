5f46b9eab68cedaad47092c1135d9d91e43e8239
metavige <metavige@gmail.com>
2014-11-04 16:02:07 +0800

    modify etcd start shell

--- twemproxy/run.sh
+++ twemproxy/run.sh
@@ -2,11 +2,12 @@
 
 # Run docker run with -e ETCD_HOST=<ip>:<port>
 if [ -n "${ETCD_HOST:+x}" ]; then
-  mv /etc/supervisor/supervisord.conf /tmp/supervisord.conf
-  sed -e "/confd -node/s/127.0.0.1:4001/${ETCD_HOST}/" /tmp/supervisord.conf > /etc/supervisor/supervisord.conf
+  mv /etc/supervisor/conf.d/twemproxy.conf /tmp/twemproxy.conf
+  sed -e "/confd -node/s/127.0.0.1:4001/${ETCD_HOST}/" /tmp/twemproxy.conf > /etc/supervisor/conf.d/twemproxy.conf
 fi
 
 # for debugging
-cat /etc/supervisor/supervisord.conf
+cat /etc/supervisor/conf.d/twemproxy.conf
 
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+# /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+/root/bin/run.sh
--- twemproxy/start_proxy.sh
+++ twemproxy/start_proxy.sh
@@ -2,11 +2,13 @@
 
 TWEMPROXY_PORT=6000
 TWEMPROXY_STATS_PORT=6001
-# ETCD_HOST=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'):4001
-ETCD_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' etcd):4001
+ETCD_HOST=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'):5001
+# ETCD_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' etcd):5001
 
-etcdctl set /services/twemproxy/port $TWEMPROXY_PORT
+#etcdctl set /services/twemproxy/port $TWEMPROXY_PORT
+curl -L $ETCD_HOST/v2/keys/services/twemproxy/port -XPOST -d value=$TWEMPROXY_PORT
 # docker rm -f redis-twemproxy
 
-docker run -d --name=redis-twemproxy -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
+docker run -d --name=redis-twemproxy -p 2223:22 -p $TWEMPROXY_PORT:6000 -p $TWEMPROXY_STATS_PORT:6222 -e ETCD_HOST=$ETCD_HOST nebula/redis-twemproxy
 # etcdctl set /services/twemproxy/host 127.0.0.1
+
